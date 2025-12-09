3ed14e0456821b67d2d4437dd56af48c7e6df183
alanab7 <alanaburgos7@gmail.com>
2025-12-09 13:17:37 -0500

    push buttons

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,16 +1,37 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Button : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   public float deadTime = 1.0f;
+//Bool used to lock down button during its set dead time 
+private bool _deadTimeActive = false;
+//public Unity Events we can use in the editor and tie other functions to. 
+public UnityEvent onPressed, onReleased;
+
+private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "Button" && !_deadTimeActive)
+        {
+            onPressed?.Invoke();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+private void OnTriggerExit(Collider other)
     {
-        
+        if(other.tag == "Button" && !_deadTimeActive)
+        {
+            onReleased?.Invoke();
+            StartCoroutine(WaitForDeadTime());
+        }
+    }
+
+    IEnumerator WaitForDeadTime()
+    {
+        _deadTimeActive = true;
+        yield return new WaitForSeconds(deadTime);
+         _deadTimeActive = false;
     }
 }
