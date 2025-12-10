296cca4b162ecc51e60ef177a00b302c4ecb3085
alanab7 <alanaburgos7@gmail.com>
2025-12-10 17:47:48 -0500

    commit w/o conflicted files

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -6,9 +6,7 @@ using UnityEngine.Events;
 public class Button : MonoBehaviour
 {
    public float deadTime = 1.0f;
-//Bool used to lock down button during its set dead time 
 private bool _deadTimeActive = false;
-//public Unity Events we can use in the editor and tie other functions to. 
 public UnityEvent onPressed, onReleased;
 
 private void OnTriggerEnter(Collider other)
