135eea87ff7c283801ff90c829577b3d9bd0b594
benisabruh <bencarterfox@yahoo.com>
2025-12-09 22:07:32 -0500

    maze stuff done

--- Assets/Scripts/Colllision Script.cs
+++ Assets/Scripts/Colllision Script.cs
@@ -1,54 +1,54 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class ColllisionScript : MonoBehaviour
-{
-    Vector3 targetPosition = new Vector3(-0.327f, -0.106f, -1.53f);
-    Vector3 direction = new Vector3(-0.327f, 4f, -1.53f);
+// public class ColllisionScript : MonoBehaviour
+// {
+//     Vector3 targetPosition = new Vector3(-0.327f, -0.106f, -1.53f);
+//     Vector3 direction = new Vector3(-0.327f, 4f, -1.53f);
  
     
-    public float speed = 1f;
+//     public float speed = 1f;
 
 
-    private Transform target;
-    public GameObject ring;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     private Transform target;
+//     public GameObject ring;
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
-    void Start()
-    {
-        Debug.Log("HI");
-        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
-        if (GetComponent<Collider>() != null){
-            Debug.Log("This exists");
-        }
+//     void Start()
+//     {
+//         Debug.Log("HI");
+//         Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+//         if (GetComponent<Collider>() != null){
+//             Debug.Log("This exists");
+//         }
         
-        //target = ring.transform;
-        //target.transform.position = new Vector3(-0.327f, -0.106f, -1.53f);
-        //Quaternion targetRotation = Quaternion.Euler(90,0,0);
-    }
+//         //target = ring.transform;
+//         //target.transform.position = new Vector3(-0.327f, -0.106f, -1.53f);
+//         //Quaternion targetRotation = Quaternion.Euler(90,0,0);
+//     }
 
-    void onTriggerEnter(Collider col){
-        if(col.tag == "wire"){
-            gameObject.moveToStart();
-        }
+//     void OnTriggerEnter(Collider col){
+//         if(col.tag == "wire"){
+//             moveToStart();
+//         }
         
-    }
-    void moveToStart(){
+//     }
+//     void moveToStart(){
         
-        GetComponent<Collider>().enabled = false;
-        transform.eulerAngles = new Vector3(90f, 0f, 0f);
-        while (gemaObject.transform.position != targetPosition /*|| this.transform.rotation != targetRotation*/){
-            float step = speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
-            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, step);
+//         GetComponent<Collider>().enabled = false;
+//         transform.eulerAngles = new Vector3(90f, 0f, 0f);
+//         while (gameObject.transform.position != targetPosition /*|| this.transform.rotation != targetRotation*/){
+//             float step = speed * Time.deltaTime;
+//             transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+//             //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, step);
             
-        }
-        Debug.Log("Movement finished");
-        GetComponent<Collider>().enabled = true;
-       // GetComponent<Rigidbody>().enabled = true;
+//         }
+//         Debug.Log("Movement finished");
+//         GetComponent<Collider>().enabled = true;
+//        // GetComponent<Rigidbody>().enabled = true;
         
-    }
+//     }
 
-    // Update is called once per frame
+//     // Update is called once per frame
 
         
-}
+// }
